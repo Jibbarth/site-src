32e80c08c08311c060b3e3a2f24a5e04224aec1b
Jibé Barth <barth.jib@gmail.com>
2021-04-10 20:55:48 +0200

    Transform gist.js into stimulus gist controllers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
-import '../bootstrap'
-
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fortawesome/fontawesome-free/js/all.min'
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+import '../bootstrap'
 
 document.addEventListener('DOMContentLoaded', () => twemoji.parse(document.body), false);
