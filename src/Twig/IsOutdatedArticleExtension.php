5484f3b5a5e9178fcb5fefc2bdda706b30a798a5
Jibé Barth <barth.jib@gmail.com>
2021-10-10 18:06:42 +0200

    Add isOutdated and timediff extensions twig

---
+++ src/Twig/IsOutdatedArticleExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Model\Article;
+use Carbon\Carbon;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class IsOutdatedArticleExtension extends AbstractExtension
+{
+    private int $minDiffInDayToBeOutdated;
+
+    public function __construct(int $minDiffInDayToBeOutdated)
+    {
+        $this->minDiffInDayToBeOutdated = $minDiffInDayToBeOutdated;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('is_outdated_article', [$this, 'isOutdated']),
+        ];
+    }
+
+    public function isOutDated(Article $article): bool
+    {
+        $articleDate = new Carbon($article->getDate());
+
+        $diff = Carbon::now()->diffInDays($articleDate);
+
+        return $diff >= $this->minDiffInDayToBeOutdated;
+    }
+}
---
+++ src/Twig/TimeDiffExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Carbon\Carbon;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class TimeDiffExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('time_diff', [$this, 'diff']),
+        ];
+    }
+
+    public function diff(\DateTimeInterface $date): string
+    {
+        return (new Carbon($date))->diffForHumans(['parts' => 2, 'join' => true]);
+    }
+}
