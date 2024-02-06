4550c9203811f982329614be4ee069b2ef024705
Jibé Barth <barth.jib@gmail.com>
2024-02-06 20:25:35 +0100

    Apply new phpcsfixer rules

--- src/Collection/SliceTrait.php
+++ src/Collection/SliceTrait.php
@@ -9,7 +9,7 @@ use Ramsey\Collection\CollectionInterface;
 trait SliceTrait
 {
     /** @phpstan-ignore-next-line */
-    public function slice(int $offset, int $length = null): CollectionInterface
+    public function slice(int $offset, ?int $length = null): CollectionInterface
     {
         $collection = clone $this;
         $collection->data = array_merge([], \array_slice($this->data, $offset, $length, true));
