4073ddcba63fb7b0e1783949f855369d3dac80bb
Jibé Barth <barth.jib@gmail.com>
2024-04-27 20:02:34 +0200

    Fix prismjs import

--- assets/controllers/code_highlight_controller.js
+++ assets/controllers/code_highlight_controller.js
@@ -1,9 +1,23 @@
 import { Controller } from '@hotwired/stimulus';
 import Prism from 'prismjs'
 
-import 'prism-themes/themes/prism-vsc-dark-plus.css'
-import '../css/gist.css'
-import '../css/markdown.css'
+import 'prismjs/components/prism-core.min.js'
+// Load the languages I need to highlight
+// To add a new one : php bin/console importmap:require prismjs/components/prism-<language>.min.js, then import it in the list
+import 'prismjs/components/prism-bash.min.js'
+import 'prismjs/components/prism-clike.min.js'
+import 'prismjs/components/prism-css.min.js'
+import 'prismjs/components/prism-diff.min.js'
+import 'prismjs/components/prism-ini.min.js'
+import 'prismjs/components/prism-javascript.min.js'
+import 'prismjs/components/prism-json.min.js'
+import 'prismjs/components/prism-markup.min.js'
+import 'prismjs/components/prism-markup-templating.min.js'
+import 'prismjs/components/prism-php.min.js'
+import 'prismjs/components/prism-twig.min.js'
+import 'prismjs/components/prism-yaml.min.js'
+
+import 'prism-themes/themes/prism-vsc-dark-plus.min.css'
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
--- importmap.php
+++ importmap.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Returns the importmap for this application.
  *
@@ -18,13 +16,85 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.4',
+    ],
+    'prismjs' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/themes/prism.min.css' => [
+        'version' => '1.29.0',
+        'type' => 'css',
+    ],
+    'prism-themes' => [
+        'version' => '1.9.0',
+    ],
+    'jsonp' => [
+        'version' => '0.2.1',
+    ],
+    'debug' => [
+        'version' => '4.3.4',
+    ],
+    'ms' => [
+        'version' => '2.1.3',
+    ],
+    'prism-themes/themes/prism-vsc-dark-plus.css' => [
+        'version' => '1.9.0',
+        'type' => 'css',
+    ],
+    'prismjs/components/prism-core' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/plugins/autoloader/prism-autoloader' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-ini.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-bash.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-css.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-diff.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-javascript.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-json.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-markup.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-php.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-twig.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-yaml.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-clike.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prismjs/components/prism-markup-templating.min.js' => [
+        'version' => '1.29.0',
+    ],
+    'prism-themes/themes/prism-vsc-dark-plus.min.css' => [
+        'version' => '1.9.0',
+        'type' => 'css',
+    ],
+    'prismjs/components/prism-core.min.js' => [
+        'version' => '1.29.0',
     ],
 ];
