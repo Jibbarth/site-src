4ab9cdcef4c4af2681b19c214633bfadb0523ed7
Jibé Barth <barth.jib@gmail.com>
2024-05-04 18:10:39 +0200

    Icon as external img for readme: Fix generating

--- src/Controller/NetworkImageController.php
+++ src/Controller/NetworkImageController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Icons\IconRegistryInterface;
 use Symplify\SymfonyStaticDumper\Contract\ControllerWithDataProviderInterface;
 use Twig\Environment;
 
@@ -13,13 +15,18 @@ final class NetworkImageController implements ControllerWithDataProviderInterfac
 {
     public function __construct(
         private Environment $twig,
+        #[Autowire(service: '.ux_icons.icon_registry')]
+        private IconRegistryInterface $iconRegistry
     ) {}
 
     #[Route('/img/fa/{type}-{collection}-{size}.svg', name: 'image')]
     public function __invoke(string $type, string $collection, int $size): Response
     {
+        $icon = $this->iconRegistry->get(sprintf('fa-%s:%s', $collection, $type));
+
         return new Response($this->twig->render('_partials/font_awesome_svg.svg.twig', [
             'size' => $size,
+            'icon' => $icon,
             'fa_ux_icon' => sprintf('fa-%s:%s', $collection, $type),
         ]), 200, ['Content-Type' => 'image/svg+xml']);
     }
