f02f4dbf2d7afbea317d89e06ba18c18020e0a6b
Jibé Barth <barth.jib@gmail.com>
2025-11-29 13:38:42 +0100

    Update commands to Invokable

--- src/Command/GenerateReadmeCommand.php
+++ src/Command/GenerateReadmeCommand.php
@@ -7,24 +7,19 @@ namespace App\Command;
 use App\Generator\ReadmeGenerator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(name: 'generate-readme')]
-final class GenerateReadmeCommand extends Command
+final class GenerateReadmeCommand
 {
-    public function __construct(private ReadmeGenerator $readmeGenerator)
-    {
-        parent::__construct();
-    }
+    public function __construct(private ReadmeGenerator $readmeGenerator) {}
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $style = new SymfonyStyle($input, $output);
+    public function __invoke(
+        SymfonyStyle $style,
+    ): int {
         $this->readmeGenerator->generate();
         $style->success('Readme successfully generated');
 
-        return self::SUCCESS;
+        return Command::SUCCESS;
     }
 }
